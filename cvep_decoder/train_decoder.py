b6ae8f697fe8820abdf63e8265e2612441d3fc15
Jordy Thielen <jordy.thielen@donders.ru.nl>
2024-09-27 18:28:27 +0200

    cleaned train_decoder

--- cvep_decoder/train_decoder.py
+++ cvep_decoder/train_decoder.py
@@ -1,194 +1,155 @@
 import json
 import os
-import pickle
-import time
+from pathlib import Path
 
-import mne
+from dareplane_utils.signal_processing.filtering import FilterBank
+from dareplane_utils.logging.logger import get_logger
+import joblib
 import numpy as np
 import pyntbci
 import pyxdf
-from dareplane_utils.signal_processing.filtering import FilterBank
-from decoder.signal_controller import SignalEmbedder
-from decoder.utils.logging import logger
-from mnelab.io import read_raw
-from scipy.signal import butter, resample, sosfilt
+from scipy.stats import resample
 
-# from ...dp-speller.speller.speller import TRIAL_TIME, PR, CODE # Imports from another module? probably not the DarePlane way? Another way of ensuring important values such a s trial_time and stimulus presentation are correct?
+from cvep_decoder.utils.logging import logger
 
-data_path = "./data/training data"
-classifier_path = "./data/classifiers"
-task = "cvep"
 
+DATA_DIR = "./data"
+DATA_FILE = "sub-001_ses-001_task-cvep_run-001_eeg.xdf"
+
+CLASSIFIER_DIR = "./data"
+CLASSIFIER_FILE = "sub-001_ses-001_task-cvep_run-001_classifier.joblib"
+
+CODE_DIR = "."
+CODE_FILE = "mgold_61_6521.npz"
+
+EEG_LSL_STREAM_NAME = "BioSemi"
+MARKER_LSL_STREAM_NAME = "marker-stream"
+
+SPELLER_MARKER_TRIAL_START = "start_trial"
+
+SEGMENT_TIME = 0.1
 TRIAL_TIME = 4.2
+
 PR = 60
-CODE = "mgold_61_6521"
+FS = 120
+L_FREQ = 2.0
+H_FREQ = 30.0
+N_CHANNELS = 32
+
+N_FOLDS = 4
+
+EVENT = "contrast"
+ONSET_EVENT = True
+ENCODING_LENGTH = 0.3
+TARGETED_ACCURACY = 0.95
 
 
-tmin = 0.0  # start of trial [s]
-tmax = TRIAL_TIME  # length of a trial [s]
-l_freq = 2.0  # high pass cutoff frequency [Hz]
-h_freq = 30.0  # low pass cutoff frequency [Hz]
-n_channels = 32  # number of channels
-fs = 120  # target sampling frequency to downsample to [Hz]
-pr = PR  # stimulus presentation rate [Hz]
-window = 1.0  # window of data to take before a trial to catch filter artefacts [s]
+def create_classifier(log_level: int = 30):
+    logger = get_logger("cvep_decoder", add_console_handler=True)
+    logger.setLevel(log_level)
 
-
-def create_classifier(subject, session, run):
-    logger.setLevel(10)
-
-    # Load EEG
-    fn = os.path.join(
-        data_path, f"sub-{subject}_ses-{session}_task-{task}_run-{run}_eeg.xdf"
-    )
-    streams = pyxdf.resolve_streams(fn)
-    names = [stream["name"] for stream in streams]
-    raw = read_raw(fn, stream_ids=[streams[names.index("BioSemi")]["stream_id"]])
-
-    # Adjust marker channel data
-    raw._data[0, :] -= np.min(raw._data[0, :])
-    raw._data[0, raw._data[0, :] > 0] = 1
-    idx = np.where(raw._data[0, :] > 0)[0]
-    idx = idx[np.concatenate(([False], np.diff(idx) < raw.info["sfreq"]))]
-    raw._data[0, idx] = 0
-
-    # Read events
-    events = mne.find_events(raw, stim_channel="Trig1", verbose=False)
-
-    # create filterbank
-    fb = FilterBank(
-        bands={"band": (l_freq, h_freq)},
-        sfreq=raw.info["sfreq"],
-        output="signal",
-        n_in_channels=len(raw.info["ch_names"]),
-        # n_in_channels=n_channels,
-        filter_buffer_s=raw.times[-1],
-    )
-
-    # data = raw.get_data()[1:33, :].T
-    data = raw.get_data().T
-
-    fb.filter(data, raw.times)
-
-    data_filtered = fb.get_data()
-    raw_f = mne.io.RawArray(data_filtered.T[0], raw.info)
-
-    # Slicing
-    # X = mne.Epochs(raw, events=events, tmin=tmin - window, tmax=tmax, baseline=None, picks="eeg", preload=True,
-    #               verbose=False).get_data(copy=True, verbose=False)
-    # logger.info(f"X shape: {X.shape} (trials x channels x samples)")
-
-    # Spectral filtering
-    # sos = butter(N=2, Wn=[l_freq, h_freq], btype='bandpass', output='sos', fs=raw.info["sfreq"])
-    # X = sosfilt(sos, X, axis=2)
-
-    # Resampling
-    # X = resample(X, int(np.round(X.shape[2] / raw.info["sfreq"] * fs)), axis=2)
-
-    # Remove 500 ms around
-    # X = X[:, :, int(window * fs):]
-    # logger.info(f"X shape: {X.shape} (trials x channels x samples)")
-    # raw.filter(l_freq=l_freq, h_freq=h_freq)
-
-    epo = mne.Epochs(
-        raw_f,
-        events=events,
-        tmin=tmin - window,
-        tmax=tmax,
-        baseline=None,
-        picks="eeg",
-        preload=True,
-        verbose=False,
-    )
-    # need resampling?
-    epo.resample(fs)
-    X = epo.get_data(tmin=tmin, tmax=tmax)
-
-    logger.info(f"X shape: {X.shape} (trials x channels x samples)")
-
-    # Extract target labels
-    fn = os.path.join(
-        data_path, f"sub-{subject}_ses-{session}_task-{task}_run-{run}_eeg.xdf"
-    )
+    # Load training file
+    fn = os.path.join(Path(DATA_DIR) / DATA_FILE)
     streams = pyxdf.load_xdf(fn)[0]
-    names = [stream["info"]["name"][0] for stream in streams]
-    stream = streams[names.index("KeyboardMarkerStream")]
+    stream_names = [stream["info"]["name"][0] for stream in streams]
+    logger.debug(f"Data loaded from {Path(DATA_DIR) / DATA_FILE}")
+
+    eeg_stream = streams[stream_names.index(EEG_LSL_STREAM_NAME)]
+    sfreq = float(eeg_stream["info"]["nominal_srate"][0])
+    data = eeg_stream["time_series"][:, 1:N_CHANNELS]
+    time = eeg_stream["time_series"] - eeg_stream["time_series"][0]
+
+    # Bandpass filter
+    fb = FilterBank(
+        bands={"band": (L_FREQ, H_FREQ)},
+        sfreq=sfreq,
+        output="signal",
+        n_in_channels=N_CHANNELS,
+        filter_buffer_s=time[-1],
+    )
+    fb.filter(data, time)
+    data = fb.get_data()
+
+    # Extract trial onsets and labels
+    marker_stream = streams[stream_names.index(MARKER_LSL_STREAM_NAME)]
+    t = []
     y = []
-    for marker in stream["time_series"]:
+    for stamp, marker in zip(marker_stream["time_stamps"], marker_stream["time_series"]):
+        if marker.startswith(SPELLER_MARKER_TRIAL_START):
+            t.append(stamp)
         try:
             marker = json.loads(marker[0])
             if isinstance(marker, dict) and "target" in marker:
                 y.append(int(marker["target"]))
         except:
             continue
+    t = np.array(t)
     y = np.array(y)
-    logger.info(f"y shape: {y.shape} (trials)")
+    logger.debug(f"Found {t.size} markers and {y.size} labels")
+
+    # Slice data to trials
+    X = np.zeros((t.size, N_CHANNELS, int(TRIAL_TIME * sfreq)))
+    for i_trial in range(t.size):
+        idx = np.argmin(np.abs(time - t[i_trial]))
+        X[i_trial, :, :] = data[idx:idx + int(TRIAL_TIME * sfreq)]
+    logger.debug(f"The training data is of shape {X.shape} before resample")
+
+    # Resample
+    X = resample(X, int(TRIAL_TIME * FS), axis=2)
+    logger.debug(f"The training data is of shape {X.shape} after resample")
 
     # Load codes
-    V = np.repeat(
-        np.load(
-            f"D:/Users/bci/bachelor_project_s1028931/ThesisSpellerProject/dp-speller/speller/codes/{CODE}.npz"
-        )["codes"].T,
-        int(fs / pr),
-        axis=1,
-    )
-    logger.info(f"V shape: {V.shape} (codes x samples)")
+    V = np.repeat(np.load(Path(CODE_DIR) / CODE_FILE)["codes"], int(FS / PR), axis=1)
+    logger.debug(f"Codes loaded from {Path(CODE_DIR) / CODE_FILE}")
+    logger.debug(f"The codes are of shape {V.shape}")
 
-    V = np.repeat(
-        np.load(f"../ThesisSpellerProject/dp-speller/speller/codes/mgold_61_6521.npz")[
-            "codes"
-        ].T,
-        int(fs / pr),
-        axis=1,
-    )
+    # Setup classifier
+    rcca = pyntbci.classifiers.rCCA(stimulus=V, fs=FS, event=EVENT, onset_event=ONSET_EVENT,
+                                    encoding_length=ENCODING_LENGTH)
+    stop = pyntbci.stopping.MarginStopping(estimator=rcca, segment_time=SEGMENT_TIME, fs=FS, target_p=TARGETED_ACCURACY)
 
     # Cross-validation
-    n_folds = 4
-    n_trials = X.shape[0]
-    folds = np.repeat(np.arange(n_folds), int(n_trials / n_folds))
-    accuracy = np.zeros(n_folds)
-    rcca = pyntbci.classifiers.rCCA(
-        stimulus=V, fs=fs, event="duration", encoding_length=0.3, onset_event=True
-    )
-    for i_fold in range(n_folds):
+    folds = np.repeat(np.arange(N_FOLDS), int(X.shape[0] / N_FOLDS))
+    accuracy = np.zeros(N_FOLDS)
+    duration = np.zeros(N_FOLDS)
+    for i_fold in range(N_FOLDS):
+
+        # Split folds
         X_trn, y_trn = X[i_fold != folds, :, :], y[i_fold != folds]
         X_tst, y_tst = X[i_fold == folds, :, :], y[i_fold == folds]
 
-        rcca.fit(X_trn, y_trn)
+        # Train classifier and stopping
+        stop.fit(X_trn, y_trn)
 
-        yh = rcca.predict(X_tst)[:, 0]
-        # print(yh)
-        # print(y_tst)
-        accuracy[i_fold] = np.mean(yh == y_tst)
+        # Loop trials
+        yh_tst = np.zeros(y_tst.size)
+        yh_dur = np.zeros(y_tst.size)
+        for i_trial in range(y_tst.size):
+            X_i = X_tst[[i_trial], :, :]
 
+            # Loop segments
+            for i_segment in range(int(X.shape[2] / (SEGMENT_TIME * FS))):
+
+                # Apply classifier
+                label = stop.predict(X_i[:, :, :int((1 + i_segment) * SEGMENT_TIME * FS)])[0]
+
+                # Stop the trial if classified
+                if label >= 0:
+                    yh_tst[i_trial] = label
+                    yh_dur[i_trial] = (1 + i_segment) * SEGMENT_TIME
+                    break
+
+        # Compute performance
+        accuracy[i_fold] = np.mean(yh_tst == y_tst)
+        duration[i_fold] = np.mean(yh_dur)
+    logger.info(f"Cross-validated accuracy of {np.mean(accuracy):.3f} +/- {np.std(accuracy):.3f}")
+    logger.info(f"Cross-validated duration of {np.mean(duration):.2f} +/- {np.std(duration):.2f}")
+
+    # Fit classifier on all data
     rcca.fit(X, y)
 
-    logger.info(f"Classifier created with accuracy: {accuracy.mean():.3f}")
-    class_loc = os.path.join(
-        classifier_path, f"rCCA_Classifier_sub-{subject}_ses-{session}.pkl"
-    )
-    with open(class_loc, "wb") as file:
-        pickle.dump(rcca, file)
-    logger.info(f"Classifier saved to {class_loc}")
+    # Save classifier
+    joblib.save(rcca, Path(CLASSIFIER_DIR) / CLASSIFIER_FILE)
+    logger.debug(f"Classifier saved to {Path(CLASSIFIER_DIR) / CLASSIFIER_FILE}")
 
     return 0
-
-
-def decode(subject, session):
-    class_loc = os.path.join(
-        classifier_path, f"rCCA_Classifier_sub-{subject}_ses-{session}.pkl"
-    )
-    with open(class_loc, "rb") as file:
-        classifier = pickle.load(file)
-    decoder = SignalEmbedder(
-        classifier=classifier,
-        input_stream_name="BioSemi",
-        output_stream_name="decoder",
-        input_window_seconds=tmax,
-        new_sfreq=fs,
-        band=(l_freq, h_freq),
-        marker_stream_name="KeyboardMarkerStream",
-        markers="start_trial",
-    )
-    decoder.init_all()
-    return decoder.run()
